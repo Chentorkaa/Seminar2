fb53248e2a3350d1e45d318abf1001e396cdbdab
Chentorka <chentorka@gmail.com>
2022-10-20 10:11:16 +0300

    GetLastDigit

---
+++ Example1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите ПО, которое принимает на вход 3-х число и на выходе показывает последнюю цифру этого числа. 
+
+int GetLastDigit(int number)
+{
+        return number%10;
+}
+Console.WriteLine("Введите число");
+int testNumber = int.Parse(Console.ReadLine()); //или прописать число
+Console.WriteLine(GetLastDigit(testNumber));
